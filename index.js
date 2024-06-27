d4d2b4f0af14b23fd02a9517eff4d231956d5097
anrichter <anrichter2@gmail.com>
2024-06-27 16:30:28 -0500

    Expanded question prompts. Filled out the writeToFile function. Lastly filled out the init function.

--- index.js
+++ index.js
@@ -1,15 +1,77 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [projectTitle, description, tableContents, instillation, usage, license, contributing, tests, gitHubURL, emailAddress];
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of the project?",
+        name: 'title'
+    }, 
+    {
+        type: "input",
+        message: "Type out a brief description of the project.",
+        name: 'description'
+    },
+    {
+        type: "input",
+        message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
+        name: "instillation",
+        default: "N/A"
+    },
+    {
+        type: "input",
+        message: "Do you have any instructions or examples of how the application should be used?", // Screenshots?
+        name: "usage",
+        default: "N/A"
+    },
+    {
+        type: "list", // How many licenses
+        message: "What license is associated with this application?",
+        name: "license",
+        choices: ["N/A", "MIT"],
+        default: "N/A"
+    },
+    {
+        type: "input",
+        message: "How would you like people to contribute to the application?",
+        name: "contribution",
+        default: "N/A"
+    },
+    {
+        type: "input",
+        message: "Are there any example tests that a user can run to prove that the application is running correctly?",
+        name: "test",
+        default: "N/A"
+    },
+    {
+        type: "input",
+        message: "What is your github URL?",
+        name: "gitHubURL"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "emailAddress"
+    }]; //tablecontents
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(`${fileName}.md`, data, error => 
+        error ? console.error(error) : console.log('Success')
+    )
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions)
+    .then(response => {
+        writeToFile(response.title, generateMarkdown(response))
+    });
+};
 
 // Function call to initialize app
 init();
--- test-readme.md
+++ test-readme.md
@@ -2,13 +2,6 @@
 
 ## Description
 
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
-
 ## Table of Contents (Optional)
 
 If your README is long, add a table of contents to make it easy for users to find what they need.
@@ -20,17 +13,7 @@ If your README is long, add a table of contents to make it easy for users to fin
 
 ## Installation
 
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
-
 ## Usage
-
-Provide instructions and examples for use. Include screenshots as needed.
-
-To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-
-    ```md
-    ![alt text](assets/images/screenshot.png)
-    ```
 
 ## License
 
@@ -40,12 +23,6 @@ The last section of a high-quality README file is the license. This lets other d
 
 🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
 
-## Badges
-
-![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
 ## How to Contribute
 
 If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
@@ -54,4 +31,4 @@ If you created an application or package and would like other developers to cont
 
 Go the extra mile and write tests for your application. Then provide examples on how to run them here.
 
-<!-- also need a questions section -->
+## Questions
