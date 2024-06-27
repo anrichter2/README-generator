afbbd0543b862dbdc1f9644dd0e9522851cf69ec
anrichter <anrichter2@gmail.com>
2024-06-27 14:37:42 -0500

    Included packages needed for the application, created array of user questions, setup the gitignore file, and made a test readme file to use in the application as the base of a temperal literal string.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [projectTitle, description, tableContents, instillation, usage, license, contributing, tests, gitHubURL, emailAddress];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
